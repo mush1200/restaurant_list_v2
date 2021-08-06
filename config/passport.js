bda7390b812ab16801593335de40f9e61e0fc26d
mush1200 <mush1200@hotmail.com>
2021-08-06 21:16:46 +0800

    feat: add message partails for login page

--- config/passport.js
+++ config/passport.js
@@ -8,15 +8,17 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
+          req.flash('warning_msg', '這個信箱尚未註冊。')
           return done(null, false, { message: 'That email is not registered!' })
         }
         return bcrypt.compare(password, user.password)
           .then(isMatch => {
             if (!isMatch) {
+              req.flash('warning_msg', '信箱或密碼輸入不正確。')
               return done(null, false, { message: 'Email or Password incorrect.' })
             }
             return done(null, user)
